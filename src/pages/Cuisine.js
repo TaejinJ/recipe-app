330b1103e8a66bf5c00c687eea88678a0ad3fde4
TaejinJ <xowls296@gmail.com>
2023-07-10 22:33:40 +0900

    모션넣기

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import {motion} from 'framer-motion';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useState } from 'react';
+
 export const Cuisine = () => {
   const [cuisine, setCuisine] = useState([]);
   let params = useParams();
@@ -21,19 +22,26 @@ useEffect(()=>{
 
 
 return (
-    <Grid>
+    <Grid
+    animate={{opacity: 1}}
+    initial={{opacity: 0}}
+    exit={{ opacity : 0}}
+    transition={{duration: 0.5}}
+    >
     {cuisine.map((item)=>{
     return(
       <Card key={item.id}>
+        <Link to={"/recipe/" + item.id}>
         <img src={item.image} alt=''/>
         <h4>{item.title}</h4>
+        </Link>
       </Card>
     )
   })}
     </Grid>
   )
 }
-    const Grid = styled.div`
+    const Grid = styled(motion.div)`
       display: grid;
       grid-template-columns: repeat(auto-fit,minmax(20rem , 1fr));
       grid-gap: 3rem;
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Home } from './Home'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes,useLocation } from 'react-router-dom'
 import { Cuisine } from './Cuisine'
 import { Searched } from './Searched'
 import { Recipe } from './Recipe'
+import { AnimatePresence } from 'framer-motion'
+import { Signup } from '../components/Signup'
 export const Pages = () => {
+    const location= useLocation();
     return (
-
-        <Routes>
+        // AnimatePresence를 위한 initial 과 mode 그리고 Routes의 location/key 설정
+        <AnimatePresence  initial={false}mode={'wait'}>
+        <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/cuisine/:type" element={<Cuisine />} />
             <Route path="/searched/:search" element={<Searched/>}/>
             <Route path="/recipe/:name" element={<Recipe/>}/>
         </Routes>
-
+        </AnimatePresence>
     )
 }
